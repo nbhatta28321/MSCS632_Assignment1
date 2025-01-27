ae8d35778c2ffda126e472a6c39a58d4beea80da
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2025-01-27 09:44:45 -0500

    added file

---
+++ calcSum.py
@@ -0,0 +1,8 @@
+def calculate_sum(arr):
+    total = 0  # Fixed typo
+    for num in arr:
+        total += num
+    return total
+numbers = [1, 2, 3, 4, 5]
+result = calculate_sum(numbers)
+print("Sum in Python:", result)
